a18f1198eb86d925aedb522eab91530f711625e9
Deepak Sharma <ds9124965@gmail.com>
2018-07-09 09:28:42 +0530

    Studennt from

--- SampleProject/Add Student.Designer.cs
+++ SampleProject/Add Student.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SampleProject
 {
-    partial class Form1
+    partial class Add_Student
     {
         /// <summary>
         /// Required designer variable.
@@ -28,34 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(123, 85);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Form1
+            // Add_Student
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(567, 472);
+            this.Name = "Add_Student";
+            this.Text = "Add_Student";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
-
--- SampleProject/Add Student.cs
+++ SampleProject/Add Student.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace SampleProject
 {
-    public partial class Form1 : Form
+    public partial class Add_Student : Form
     {
-        public Form1()
+        public Add_Student()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Hello");
         }
     }
 }
